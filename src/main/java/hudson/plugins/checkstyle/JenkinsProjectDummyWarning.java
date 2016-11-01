a23ed630db052b503a2d82727e4c9f0ca6b0315d
Lukas Krose <krose@hm.edu>
2016-11-01 18:09:49 +0100

    Added dummy class with style errors
    Dummy class with style errors. Used for my Bachelor Thesis

---
+++ src/main/java/hudson/plugins/checkstyle/JenkinsProjectDummyWarning.java
@@ -0,0 +1,15 @@
+package hudson.plugins.checkstyle;
+
+/**
+ * This class is a dummy class to provide changes in the current git revision.
+ * This is used for my Bachelor thesis and will be removed.
+ *
+ * Created by Lugge on 01.11.2016.
+ */
+public class JenkinsProjectDummyWarning {
+
+    public void dummyMethodWithStyleErrors(){
+                        boolean wrongIndentation = true;
+        return;
+    }
+}
